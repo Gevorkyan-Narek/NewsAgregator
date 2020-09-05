61324f7ef8fa4138573a1079c23272b8f8f7a095
Narek Gevorkyan <mrnarek149@gmail.com>
2020-09-05 13:05:08 +0400

    Реализован Bottom Sheet

---
+++ app/src/main/java/com/cyclone/newsagregator/Link.kt
@@ -0,0 +1,7 @@
+package com.cyclone.newsagregator
+
+data class Link (
+    val name: String,
+    var isEnable: Boolean,
+    val href: String
+)
--- app/src/main/java/com/cyclone/newsagregator/MainActivity.kt
+++ app/src/main/java/com/cyclone/newsagregator/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.cyclone.newsagregator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val links = arrayListOf(
+            Link("4PDA", true, "4pda.ru"),
+            Link("Habr", true, "habr.ru"),
+            Link("SSAU", false, "ssau.ru")
+        )
+
+        val bottomSheetBehavior = BottomSheetBehavior.from(layout_bottom_sheet)
+        bottomSheetBehavior.isHideable = false
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        Log.d("State:", "Collapsed")
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        Log.d("State:", "Expanded")
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+
+                    }
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+
+                    }
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+        })
+
+        links_recycler.adapter = LinksAdapter(links)
     }
 }
